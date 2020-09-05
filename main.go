1650cb07cb43030f8c826c833b9d690e4bfd1d59
Fernando Gelin <fernando@fernandogelin.com>
2020-09-05 08:26:53 -0400

    feat: add update and delete

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/fernandogelin/golang-api/models" 
 	"github.com/fernandogelin/golang-api/controllers" 
@@ -10,14 +11,17 @@ import (
 func main() {
 	r := gin.Default()
 
+	models.ConnectDatabase()
+
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"data": "hello world"})
 	})
 
-	models.ConnectDatabase()
-
   r.GET("/books", controllers.FindBooks)
 	r.POST("/books", controllers.CreateBook)
 
+  r.GET("/books/:id", controllers.FindBook) 
+	r.PATCH("/books/:id", controllers.UpdateBook)
+	r.DELETE("/books/:id")
 	r.Run()
 }
